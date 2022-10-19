6d22584fdbc76824eb466c8198b4b894ab867561
Neel Pawar <81655816+neelpawarcmu@users.noreply.github.com>
2022-10-19 17:01:02 -0400

    remove \n's from individual answers

--- bertdemo.js
+++ bertdemo.js
@@ -1,12 +1,12 @@
 class Demo {
     constructor() {
-        return null;
+        this.model = null;
+        return
     }
 
     // save model for accessibility from methods and console
     initModel(model) {
         this.model = model;
-        // this.answers = new Array();
         console.log('model loaded');
         document.getElementById('loading-icon').style.display = "none";
     }
@@ -14,23 +14,54 @@ class Demo {
     answerQuestion() {
         const question = document.getElementById('question').value,
               passage = document.getElementById('passage').value,
-              answerTextBox = document.getElementById('answer-textbox');
+              answersTextBox = document.getElementById('answer-textbox');
 
-        let answerText = '';
+        let answersText = '';
 
         this.model.findAnswers(question, passage).then(answers => {
             this.answers = answers;
             answers.forEach((ans, idx) => {
-                answerText += `${idx}: ${ans.text} score: ${ans.score}\n`
+                const ansText = ans.text.replace(/(\r\n|\n|\r)/gm, " ");
+                answersText += `${idx}: ${ansText} | score: ${ans.score}\n`;
             });
-            console.log(answers);
-            answerTextBox.value = this.answers.length > 0 ? answerText : 'no predictions';
+            answersTextBox.value = this.answers.length > 0 ? answersText : 'no predictions';
         });
     }
-}
+
+// trial code for plotly based heatmaps
+//     plotWeights(newPlot = false) {
+//         const rawData = this.model.model.artifacts.weightData
+//         const array = [...new Uint8Array(rawData).slice(4096,8192)]
+//         const weights = [];
+//         while (array.length) weights.push(array.splice(0,64));
+//         console.log('weights:\n', weights);
+//         const data = [
+//             {
+//                 z: weights,
+//                 type: "heatmap",
+//             }
+//         ];
+//         const layout = {
+//             title: {text: "Weights"},
+//         };
+//         if (newPlot) {
+//             Plotly.newPlot("heatmap", data, layout);
+//             const heatmap = document.getElementById("heatmap");
+//         } else {
+//             Plotly.react("heatmap", data, layout);
+//         }
+//     }
+// }
+
+// class Plots extends Demo {
+//     constructor() {
+//         super();
+//     }
+// }
 
 // TODO: main fn
 const demo = new Demo();
+// const plots = new Plots();
 
 // Load model
 // Notice there is no 'import' statement. 'qna' and 'tf' is
